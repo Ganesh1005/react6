8877e1b40ffddc86f8758f9784cf5fe7a51ef001
Jaya Ganesh <jayaganesh05@gmail.com>
2023-10-29 22:16:45 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Hii there it's me Ganesh </h1>
+      <p>I am a Web Developer</p>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      I created many cool apps like calculators,colour changers and many more
+    </section>
+  );
+}
+
+export default Projects;
